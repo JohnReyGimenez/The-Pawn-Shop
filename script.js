a1176e970423908eac5f82b253d56eabb3730e1d
JohnReyGimenez <gimenezjr5@gmail.com>
2024-10-11 13:36:00 +0800

    added eventlisteners and showsection funtion

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    document.querySelector("#startgamebtn").addEventListener("click", function() {
+        showSection('game');
+    });
+
+    document.querySelector("#dashboardbtn").addEventListener("click", function() {
+        showSection('dashboard')
+    });
+
 });
+
+function showSection () {
+    
+}
